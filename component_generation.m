c1fb3466995fd3dcc2104026622a786abd52b183
Manu Krishnamurthy <88797743+mkrishnamu@users.noreply.github.com>
2021-09-30 14:57:46 -0400

    Update component_generation.m

--- component_generation.m
+++ component_generation.m
@@ -1,23 +1,31 @@
-function component_generation(fslpath, fmriprepdir, numcomps)
-if nargin==0
-    fslpath = '';
-    fmriprepdir = 'Data/';
-    numcomps = {'auto', '20', '30', '40', '50', '60'};
-end
+function component_generation_wMCM(fslpath, fmriprepdir, numcomps, smoothkernel)
 cd(fmriprepdir)
 inputsubs=dir(fmriprepdir);
 subs = {inputsubs.name};
 for subi = 1:length(subs)
+    tic;
     disp(char(subs(subi)))
-    inputfilename = dir([[char(subs(subi)) '/rest/'], '*_task-rest*_space-T1w_desc-preproc_bold.nii.gz']);
-    if ~isempty(inputfilename)
+    inputfilename = dir([char(subs(subi)) '/func/' char(subs(subi)) '_task-rest*_space-T1w_desc-preproc_bold.nii.gz']);
+    if ~isempty(inputfilename)            
+        inputfilename = [inputfilename.folder '/' inputfilename.name];
+        smoothfilename = [char(subs(subi)) '/func/' char(subs(subi)) '_task-rest_space-T1w_desc-preproc_bold_smooth.nii.gz'];
+        if ~isfile(smoothfilename)
+            smoothstring = [fslpath '/fslmaths ' inputfilename ' -kernel gauss ' smoothkernel ' -fmean ' smoothfilename];
+            system(smoothstring);
+        end
         for numi = numcomps
-            outdir = [char(subs(subi)) '/restmelodic_' char(numi)];
-            numICAstring = [fslpath '/bin/melodic -i ' char(subs(subi)) '/rest/' inputfilename ' --dim=' char(numi) ' -o ' outdir];
+            if strcmp(char(numi), 'auto')
+                outdir = [char(subs(subi)) '/restmelodicsmooth_' char(numi)];
+                numICAstring = [fslpath '/melodic -i ' smoothfilename ' -o ' outdir];
+            else
+                outdir = [char(subs(subi)) '/restmelodicsmooth_' char(numi)];
+                numICAstring = [fslpath '/melodic -i ' smoothfilename ' --dim=' char(numi) ' -o ' outdir];
+            end
             if ~exist([outdir '/melodic_IC.nii.gz'], 'file')
                 system(numICAstring);
             end
         end
     end
+    toc;
 end
 end
