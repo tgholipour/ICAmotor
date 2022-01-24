a673adcf91439ea3bbc41cdc10b4b8d583f10364
Manu Krishnamurthy <88797743+mkrishnamu@users.noreply.github.com>
2022-01-24 13:06:21 -0500

    Update wMCM_component_generation.m

--- wMCM_component_generation.m
+++ wMCM_component_generation.m
@@ -1,25 +1,25 @@
 function wMCM_component_generation(fslpath, fmriprepdir, numcomps, smoothkernel)
-cd(fmriprepdir)
 inputsubs=dir(fmriprepdir);
 subs = {inputsubs.name};
+subs = subs(3:length(subs));
 for subi = 1:length(subs)
     tic;
     disp(char(subs(subi)))
-    inputfilename = dir([char(subs(subi)) '/func/' char(subs(subi)) '_task-rest*_space-T1w_desc-preproc_bold.nii.gz']);
+    inputfilename = dir([fmriprepdir '/' char(subs(subi)) '/func/' char(subs(subi)) '_task-rest*_space-T1w_desc-preproc_bold.nii.gz']);
     if ~isempty(inputfilename)            
         inputfilename = [inputfilename.folder '/' inputfilename.name];
-        smoothfilename = [char(subs(subi)) '/func/' char(subs(subi)) '_task-rest_space-T1w_desc-preproc_bold_smooth.nii.gz'];
+        smoothfilename = [fmriprepdir '/' char(subs(subi)) '/func/' char(subs(subi)) '_task-rest_space-T1w_desc-preproc_bold_smooth.nii.gz'];
         if ~isfile(smoothfilename)
-            smoothstring = [fslpath '/fslmaths ' inputfilename ' -kernel gauss ' smoothkernel ' -fmean ' smoothfilename];
+            smoothstring = ['export FSLDIR=' fslpath '; . ${FSLDIR}/etc/fslconf/fsl.sh; ' fslpath '/bin/fslmaths ' inputfilename ' -kernel gauss ' num2str(smoothkernel) ' -fmean ' smoothfilename];
             system(smoothstring);
         end
         for numi = numcomps
             if strcmp(char(numi), 'auto')
-                outdir = [char(subs(subi)) '/restmelodicsmooth_' char(numi)];
-                numICAstring = [fslpath '/melodic -i ' smoothfilename ' -o ' outdir];
+                outdir = [fmriprepdir '/' char(subs(subi)) '/restmelodicsmooth_' char(numi)];
+                numICAstring = ['export FSLDIR=' fslpath '; . ${FSLDIR}/etc/fslconf/fsl.sh; ' fslpath '/bin/melodic -i ' smoothfilename ' -o ' outdir];
             else
-                outdir = [char(subs(subi)) '/restmelodicsmooth_' char(numi)];
-                numICAstring = [fslpath '/melodic -i ' smoothfilename ' --dim=' char(numi) ' -o ' outdir];
+                outdir = [fmriprepdir '/' char(subs(subi)) '/restmelodicsmooth_' char(numi)];
+                numICAstring = ['export FSLDIR=' fslpath '; . ${FSLDIR}/etc/fslconf/fsl.sh; ' fslpath '/bin/melodic -i ' smoothfilename ' --dim=' char(numi) ' -o ' outdir];
             end
             if ~exist([outdir '/melodic_IC.nii.gz'], 'file')
                 system(numICAstring);
