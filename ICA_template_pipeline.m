b4f9b93b6e47d4f36c6748a313a013a51323cf05
Manu Krishnamurthy <88797743+mkrishnamu@users.noreply.github.com>
2022-05-12 08:30:20 -0400

    Update ICA_template_pipeline.m

--- ICA_template_pipeline.m
+++ ICA_template_pipeline.m
@@ -10,6 +10,7 @@ if(nargin==0)
     comp_sel_thresholds = [1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6]; %list of thresholds used to select components that comprise the final map
     numcomps = {'auto', '20', '30', '40', '50', '60'}; %list of Total Number of Components used to select components that comprise the final map
     templatepath = 'Templates/HMAT_pre_post_central.nii';
+    templateMNIspace = 'MNI152NLin6Asym';
     firstlevdir = 'FirstLevel/';
     comp_val_thresholds = [1 1.5 2 2.5 3 3.5 4 4.5 5 5.5 6];%List of ICA map thresholds used to compare to task activation
     activ_val_thresholds = [ 0.05 0.01 0.005 0.001];%List of task activation p value thresholds (assuming task activation is a t map)
@@ -20,6 +21,6 @@ cd(maindir);
 addpath(spmpath);
 addpath(meanshiftclusterfolderpath);
 wIMM_component_generation(fslpath, fmriprepdir, numcomps, icasmoothkernel);
-wIMM_creation(antspath, afnipath, fmriprepdir, templatepath, comp_sel_thresholds, numcomps);
+wIMM_creation(antspath, afnipath, fmriprepdir, templatepath, comp_sel_thresholds, numcomps, templateMNIspace);
 wIMM_task_validation(fmriprepdir, comp_val_thresholds, activ_val_thresholds, taskname, firstlevdir, afnipath);
 end
